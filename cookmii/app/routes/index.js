e20d418f775a264334f1f6252cdd62d294842fa3
Michael Schwarz <michael@moviepilot.com>
2014-12-24 00:11:08 +0100

    List best recipes, ingredients and categories

--- cookmii/app/routes/index.js
+++ cookmii/app/routes/index.js
@@ -1,24 +1,65 @@
 module.exports = function(app){
   require('./users')(app);
   require('./recipes')(app);
+  var async = require("async");
 
   var Recipe = app.locals.Recipe;
 
   app.route('/').get(function (req, res){
-    Recipe.model.aggregate(
-      {$unwind: '$ingredients'},
-      {$group : {_id : "$ingredients.name", count : {$sum : 1}}},
-      {$sort:{count:-1}},
-      {$limit: 10},
-      function(err, ingredients){
-        res.render('index',{
-          ingredients:ingredients
+    var categories;
+    var ingredients;
+    var bestRecipes;
+    async.parallel([
+      function(callback){
+        Recipe.model.aggregate(
+          {$unwind: '$ingredients'},
+          {$group : {_id : "$ingredients.name", count : {$sum : 1}}},
+          {$sort:{count:-1}},
+          {$limit: 10},
+          function(err, ingr){
+            if(err)
+              callback(err);
+            ingredients = ingr;
+            callback();
+          }
+        );
+      },
+      function(callback){
+        Recipe.model.aggregate(
+          {$unwind: '$categories'},
+          {$group : {_id : "$categories.name", count : {$sum : 1}}},
+          {$sort:{count:-1}},
+          {$limit: 10},
+          function(err, cat){
+            if(err)
+              callback(err);
+            categories = cat;
+            callback();
+          }
+        );
+      },
+      function(callback){
+        // WHY __v ???
+        Recipe.model
+        .find({isPublic:true})
+        .sort({__v:-1})
+        .limit(5)
+        .exec(function(err,best){
+          if(err)
+            throw err;
+          bestRecipes = best;
+          callback();
         });
       }
-    );
-  });
-
-  app.route('/').get(function (req, res){
-    res.redirect('/recipes');
+    ],
+    function(err){
+      if(err)
+        throw err;
+      res.render('index',{
+        ingredients: ingredients,
+        categories: categories,
+        bestRecipes: bestRecipes
+      });
+    });
   });
 }
